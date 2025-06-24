c9104e8c5cd2834d5e71b2c8c116133fb408449f
jeooa <joemarlabendia4@gmail.com>
2025-06-24 21:40:28 +0800

    5th commit

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const chatRoutes = require('./routes/chat');
-
+const PORT = 3000;
 const app = express();
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/chat', chatRoutes);
 
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
